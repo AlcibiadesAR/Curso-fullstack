3ed9bee21a2d165d0a66fd167991b383121a29ca
AlcibiadesAR <alcibiades.acosta.r@gmail.com>
2024-07-02 14:52:55 -0500

    javascript asincrono

---
+++ Javascript-DOM/JS-Asincronico/script.js
@@ -0,0 +1,43 @@
+// Primer acercamiento a fetch
+
+/*fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+.then(resp => resp.json())
+.then(json => console.log(json))*/
+
+
+//async/ await
+
+/*const promise = async() => {
+
+const promiseText = new Promise(resolve => {
+
+    setTimeout(() => {
+    resolve('Hola Mundo')
+    }, 3000);
+});
+
+    document.getElementById('loading').innerHTML = await promiseText
+}
+
+
+promise();*/
+
+// async/ await con fetch
+
+
+const comic = async () =>{
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments');
+        
+        if (!response.ok) {
+            throw new Error('¡Hubo un problema llamando a la API')
+        }
+        const data = await response.json();
+        document.getElementById('response').innerHTML = data[4].email
+
+    }catch (error){
+        document.getElementById('response').innerHTML = error
+    }
+}
+
+comic();
--- Javascript-DOM/Promesas/script.js
+++ Javascript-DOM/Promesas/script.js
@@ -1,4 +1,6 @@
-const number = prompt("Ingrese un número del 1 al 10");
+// opcion 1
+
+/*const number = prompt("Ingrese un número del 1 al 10");
 
 const myPromise = new Promise((resolve, reject) => {
   const random = Math.round(Math.random() * 10);
@@ -21,4 +23,57 @@ myPromise.then((result) => {
 }).catch((err) => {
     document.getElementById('loading').innerHTML = ''
     document.getElementById('response').innerHTML = err
+});*/
+
+//opcion 2
+
+/*const MyPromise2 = new Promise((resolve, reject) => {
+    const random = Math.round(Math.random()*10);
+    setTimeout( ()=>{
+        resolve(random);
+    }, 2000)
 });
+
+const isEven = (number) => {
+   const even = number % 2 == 1 ? 'es un impar' : 'es par';
+   document.getElementById('loading').innerHTML = ''
+   document.getElementById('response').innerHTML = `El número ${number} ${even}`
+}
+
+document.getElementById('loading').innerHTML = 'cargando...'
+
+MyPromise2.then(isEven).catch(err => {
+    document.getElementById('loading').innerHTML = ''
+    document.getElementById('response').innerHTML = err;
+});*/
+
+//0pcion 3
+
+const promise1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res(`La promesa 1 fue un exito`);
+  }, 5000);
+});
+
+const promise2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res(`La promesa 2 fue un exito`);
+  }, 3000);
+});
+
+const promise3 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res(`La promesa 3 fue un exito`);
+  }, 2000);
+});
+
+Promise.all([promise1, promise2, promise3])
+  .then((res) => {
+    document.getElementById("loading").innerHTML = "";
+    document.getElementById(
+      "response"
+    ).innerHTML = `${res[0]} <br> ${res[1]} <br> ${res[2]}`;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
